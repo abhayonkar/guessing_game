8d9b4d0d69dc64f5d8602a5cd756c9dc4e9e2c67
abhayonkar <abhayronkar@gmail.com>
2024-05-09 23:59:39 +0530

    First program

---
+++ guessing_game.ex
@@ -0,0 +1,29 @@
+defmodule GuessingGame do
+  #guess between a low number and a high number -> so guess a middle number
+  # tell user our guess
+  # yes -> game is over
+  # "bigger" -> bigger(low, high)
+  # "smaller" -> smaller(low, high)
+  # anything else -> ask to enter a valid reponse
+  def guess(low, high) do
+    # IO.puts low
+    # IO.puts high
+    # IO.puts mid(low, high)
+    IO.puts ("the number you have guessed is #{mid(low, high)}?\n")
+  end
+
+  def mid(low, high) do
+    div(low + high, 2)
+  end
+
+  def bigger(low, high) do
+    new_low = min(high, mid(low, high) + 1)
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
+  end
+
+end
